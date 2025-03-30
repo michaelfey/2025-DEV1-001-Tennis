ee45d1b4517732a89aecfa4fe23ce66a5643e98b
michaelfey <michael.feyaerts@gmail.com>
2025-03-30 23:11:25 +0200

    add spring boot to the app

---
+++ src/main/java/be/kata/tennis/application/TennisApplication.java
@@ -0,0 +1,11 @@
+package be.kata.tennis.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TennisApplication.class, args);
+    }
+}
