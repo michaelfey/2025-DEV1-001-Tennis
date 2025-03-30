9e0c27eddc080d94dec5d6f0c0ed7a900798f73d
michaelfey <michael.feyaerts@gmail.com>
2025-03-30 15:47:44 +0200

    refactor test: replace for loop with stream when simulating tennis game

--- src/test/java/be/kata/tennis/TennisGameTest.java
+++ src/test/java/be/kata/tennis/TennisGameTest.java
@@ -3,6 +3,8 @@ package be.kata.tennis;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TennisGameTest {
@@ -18,61 +20,61 @@ public class TennisGameTest {
 
     @Test
     void whenGameStarted_thenScoreShouldBeLoveLove() {
-        simulateTennisGame(0,0);
+        simulateTennisGame(0, 0);
         assertThat(tennisGame.getScore()).isEqualTo("Love - Love");
     }
 
     @Test
     void givenScoreIsLoveLove_whenPlayer1Scores_ThenScoreIsFifteenLove() {
-        simulateTennisGame(1,0);
+        simulateTennisGame(1, 0);
 
         assertThat(tennisGame.getScore()).isEqualTo("Fifteen - Love");
     }
 
     @Test
     void givenScoreIsFifteenLove_whenPlayer1Scores_ThenScoreIsThirtyLove() {
-        simulateTennisGame(2,0);
+        simulateTennisGame(2, 0);
 
         assertThat(tennisGame.getScore()).isEqualTo("Thirty - Love");
     }
 
     @Test
     void whenPlayer1Scores3Points_ThenScoreIsFortyLove() {
-        simulateTennisGame(3,0);
+        simulateTennisGame(3, 0);
 
         assertThat(tennisGame.getScore()).isEqualTo("Forty - Love");
     }
 
     @Test
     void whenPlayer2Scores1Point_ThenScoreIsLoveFifteen() {
-        simulateTennisGame(0,1);
+        simulateTennisGame(0, 1);
 
         assertThat(tennisGame.getScore()).isEqualTo("Love - Fifteen");
     }
 
     @Test
     void whenPlayer2Scores2Points_ThenScoreIsLoveThirty() {
-        simulateTennisGame(0,2);
+        simulateTennisGame(0, 2);
 
         assertThat(tennisGame.getScore()).isEqualTo("Love - Thirty");
     }
 
     @Test
     void whenPlayer2Scores3Points_ThenScoreIsLoveForty() {
-        simulateTennisGame(0,3);
+        simulateTennisGame(0, 3);
 
         assertThat(tennisGame.getScore()).isEqualTo("Love - Forty");
     }
 
     @Test
     void whenBothPlayersReach3points_thenScoreIsDeuce() {
-        simulateTennisGame(3,3);
+        simulateTennisGame(3, 3);
         assertThat(tennisGame.getScore()).isEqualTo("Deuce");
 
     }
 
     private void simulateTennisGame(int player1Points, int player2Points) {
-        for (int i = 0; i < player1Points; i++) this.tennisGame.scorePoint(PLAYER_1);
-        for (int i = 0; i < player2Points; i++) this.tennisGame.scorePoint(PLAYER_2);
+        IntStream.range(0, player1Points).forEach(i -> tennisGame.scorePoint(PLAYER_1));
+        IntStream.range(0, player2Points).forEach(i -> tennisGame.scorePoint(PLAYER_2));
     }
 }
