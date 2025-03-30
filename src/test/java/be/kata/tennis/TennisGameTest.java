adc4a81992a8d350c3696a0451a4820f634bd28a
michaelfey <michael.feyaerts@gmail.com>
2025-03-30 15:20:53 +0200

    refactor tests for easier creation of test scenarios

--- src/test/java/be/kata/tennis/TennisGameTest.java
+++ src/test/java/be/kata/tennis/TennisGameTest.java
@@ -1,34 +1,43 @@
 package be.kata.tennis;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TennisGameTest {
 
+    private TennisGame tennisGame;
+    private static final String PLAYER_1 = "Heisenberg";
+    private static final String PLAYER_2 = "Gustavo Fring";
+
+    @BeforeEach
+    void before() {
+        this.tennisGame = new TennisGameImpl(PLAYER_1, PLAYER_2);
+    }
 
     @Test
     void whenGameStarted_thenScoreShouldBeLoveLove() {
-        TennisGame tennisGame = new TennisGameImpl("Heisenberg", "Gustavo Fring");
+        simulateTennisGame(0,0);
         assertThat(tennisGame.getScore()).isEqualTo("Love - Love");
     }
 
     @Test
     void givenScoreIsLoveLove_whenPlayer1Scores_ThenScoreIsFifteenLove() {
-        TennisGame tennisGame = new TennisGameImpl("Heisenberg", "Gustavo Fring");
-
-        tennisGame.scorePoint("Heisenberg");
+        simulateTennisGame(1,0);
 
         assertThat(tennisGame.getScore()).isEqualTo("Fifteen - Love");
     }
 
     @Test
     void givenScoreIsFifteenLove_whenPlayer1Scores_ThenScoreIsThirtyLove() {
-        TennisGame tennisGame = new TennisGameImpl("Heisenberg", "Gustavo Fring");
-        tennisGame.scorePoint("Heisenberg");
-
-        tennisGame.scorePoint("Heisenberg");
+        simulateTennisGame(2,0);
 
         assertThat(tennisGame.getScore()).isEqualTo("Thirty - Love");
+    }
+
+    private void simulateTennisGame(int player1Points, int player2Points) {
+        for (int i = 0; i < player1Points; i++) this.tennisGame.scorePoint(PLAYER_1);
+        for (int i = 0; i < player2Points; i++) this.tennisGame.scorePoint(PLAYER_2);
     }
 }
