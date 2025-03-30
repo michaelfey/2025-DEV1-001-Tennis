0984479278b7b6c36b390ec84b90eef0ddc41376
michaelfey <michael.feyaerts@gmail.com>
2025-03-30 14:47:46 +0200

    second test case: add failing test

--- src/test/java/be/kata/tennis/TennisGameTest.java
+++ src/test/java/be/kata/tennis/TennisGameTest.java
@@ -6,9 +6,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TennisGameTest {
 
+
     @Test
     void whenGameStarted_thenScoreShouldBeLoveLove() {
         TennisGame tennisGame = new TennisGameImpl("Heisenberg", "Gustavo Fring");
         assertThat(tennisGame.getScore()).isEqualTo("Love - Love");
+    }
+
+    @Test
+    void givenScoreIsLoveLove_whenPlayer1Scores_ThenScoreIsFifteenLove() {
+        TennisGame tennisGame = new TennisGameImpl("Heisenberg", "Gustavo Fring");
+        tennisGame.scorePoint("Heisenberg");
+        assertThat(tennisGame.getScore()).isEqualTo("Fifteen - Love");
     }
 }
